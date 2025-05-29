a7294f56cd56a13076d12096022be8d9925666bc
Ghost <azmeerfarhan786@gmail.com>
2025-05-29 20:44:40 +0500

    Bit Magic Done

---
+++ Bit Manipulation (16-20)/Day20/Day20Q5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0, n = nums.size();
+        for(int i = 0;i < n;i++){
+            ans ^= i;
+            ans ^= nums[i];
+        }
+        ans ^= n;
+        return ans;
+    }
+};
